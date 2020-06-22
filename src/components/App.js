4fa9dafc9327071e53d336d6219af5207f877cd1
ThreeSnakes <dev.select995@gmail.com>
2020-06-22 22:57:27 +0900

    DELETE: useless code
    - 쓸모없는 파일 삭제.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../style/App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <header classname="App-header">
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App;
